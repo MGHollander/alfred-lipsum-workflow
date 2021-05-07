e38474b1e9525654f73b944ff9dd1c19565ea504
Marc Hollander <marchollander@gmail.com>
2021-05-07 20:37:59 +0200

    Add PHPdoc block and update formatting for phpcs

--- script.php
+++ script.php
@@ -1,5 +1,17 @@
 <?php
-include_once('lib/LoremIpsum.php');
+/**
+ * Lorem ipsum generator for Alfred
+ *
+ * PHP version 7
+ *
+ * @category Alfred
+ * @package  AlfredLipsumWorkflow
+ * @author   Marc Hollander <marchollander@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/MGHollander/alfred-lipsum-workflow
+ */
+
+require_once 'lib/LoremIpsum.php';
 
 $lipsum = new joshtronic\LoremIpsum();
 
@@ -13,53 +25,55 @@ $length = $length ? $length : 1;
  *
  * Generates characters of lorem ipsum.
  *
- * @param  integer $count how many characters to generate
+ * @param integer $count how many characters to generate
+ *
  * @return string generated lorem ipsum characters
  */
-function characters($count) {
-  global $lipsum;
+function characters($count)
+{
+    global $lipsum;
 
-  $character_count = 0;
-  $words_count = 0;
-  $words_array = array();
+    $character_count = 0;
+    $words_count = 0;
+    $words_array = array();
 
-  while ($character_count < $count) {
-    $word = $lipsum->word();
-    $word_length = strlen($word);
+    while ($character_count < $count) {
+        $word = $lipsum->word();
+        $word_length = strlen($word);
 
-    if ($words_count === 0 || $word !== $words_array[$words_count - 1]) {
-      $character_count += $word_length + count($words_array);
+        if ($words_count === 0 || $word !== $words_array[$words_count - 1]) {
+            $character_count += $word_length + count($words_array);
 
-      array_push($words_array, $word);  
+            array_push($words_array, $word);
 
-      $words_count = count($words_array);
+            $words_count = count($words_array);
+        }
     }
-  }
 
-  $words = implode(' ', $words_array);
-  $words = substr($words, 0, $count);
+    $words = implode(' ', $words_array);
+    $words = substr($words, 0, $count);
 
-  $last_character = substr($words, -1);
+    $last_character = substr($words, -1);
 
-  if ($last_character === ' ') {
-    $words = substr($words, 0, -1) . substr($lipsum->word(), 0, 1);
-  }
+    if ($last_character === ' ') {
+        $words = substr($words, 0, -1) . substr($lipsum->word(), 0, 1);
+    }
 
-  return $words;
+    return $words;
 }
 
 switch ($type) {
-  case 'characters':
-    $output = characters($length);
+case 'characters':
+        $output = characters($length);
     break;
-  case 'words':
-    $output = $lipsum->words($length);
+case 'words':
+        $output = $lipsum->words($length);
     break;
-  case 'sentences':
-    $output = $lipsum->sentences($length);
+case 'sentences':
+        $output = $lipsum->sentences($length);
     break;
-  case 'paragraphs':
-    $output = $lipsum->paragraphs($length);
+case 'paragraphs':
+        $output = $lipsum->paragraphs($length);
     break;
 }
 
